0002f0831d26073c5ac4ef4b8848385148a30d1c
towhid0845 <towhid0845@gmail.com>
2024-04-21 13:29:09 +0600

    news list page

---
+++ notice-list.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- head part start -->
+<?php include("./component/head.php"); ?>
+<!-- head part end -->
+
+<body>
+
+  <!-- navbar part start -->
+  <?php include("./component/menu.php"); ?>
+  <!-- navbar part end -->
+
+
+  <!-- breadcrumb part start -->
+  <section class="bread-crumb">
+    <div class="overlay">
+      <div class="container">
+        <div class="text text-center">
+          <h3>Notice List</h3>
+          <ul class="list-unstyled">
+            <li>
+              <a href="index.html"><i class="fas fa-home"></i> Home</a>
+            </li>
+            <li>Notice List</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- breadcrumb part end -->
+
+
+  <!-- photo gallery page start -->
+  <section class="notice notice_page">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+
+          <div class="search-block">
+            <div class="search">
+              <div class="input-groups">
+                <input type="text" placeholder="Search here...">
+                <button>
+                  <i class="fas fa-search"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-12 mb-3">
+          <ul class="d-flex align-items-center position-relative list-unstyled">
+            <li class="date text-center px-2">
+              17 Nov 2023 <span class="d-block"></span>
+            </li>
+            <li class="info"><a href="#" target="_blank">Lorem ipsum dolor sit amet consectetur</a>
+            </li>
+            <li class="pdf">
+              <a href="#" target="_blank"><img src="./images/pdf.png" alt=""></a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-12 mb-3">
+          <ul class="d-flex align-items-center position-relative list-unstyled">
+            <li class="date text-center px-2">
+              17 Nov 2023 <span class="d-block"></span>
+            </li>
+            <li class="info"><a href="#" target="_blank">Lorem ipsum dolor sit amet consectetur</a>
+            </li>
+            <li class="pdf">
+              <a href="#" target="_blank"><img src="./images/pdf.png" alt=""></a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-12 mb-3">
+          <ul class="d-flex align-items-center position-relative list-unstyled">
+            <li class="date text-center px-2">
+              17 Nov 2023 <span class="d-block"></span>
+            </li>
+            <li class="info"><a href="#" target="_blank">Lorem ipsum dolor sit amet consectetur</a>
+            </li>
+            <li class="pdf">
+              <a href="#" target="_blank"><img src="./images/pdf.png" alt=""></a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-12 mb-3">
+          <ul class="d-flex align-items-center position-relative list-unstyled">
+            <li class="date text-center px-2">
+              17 Nov 2023 <span class="d-block"></span>
+            </li>
+            <li class="info"><a href="#" target="_blank">Lorem ipsum dolor sit amet consectetur</a>
+            </li>
+            <li class="pdf">
+              <a href="#" target="_blank"><img src="./images/pdf.png" alt=""></a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-12 mb-3">
+          <ul class="d-flex align-items-center position-relative list-unstyled">
+            <li class="date text-center px-2">
+              17 Nov 2023 <span class="d-block"></span>
+            </li>
+            <li class="info"><a href="#" target="_blank">Lorem ipsum dolor sit amet consectetur</a>
+            </li>
+            <li class="pdf">
+              <a href="#" target="_blank"><img src="./images/pdf.png" alt=""></a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-12 mb-3">
+          <ul class="d-flex align-items-center position-relative list-unstyled">
+            <li class="date text-center px-2">
+              17 Nov 2023 <span class="d-block"></span>
+            </li>
+            <li class="info"><a href="#" target="_blank">Lorem ipsum dolor sit amet consectetur</a>
+            </li>
+            <li class="pdf">
+              <a href="#" target="_blank"><img src="./images/pdf.png" alt=""></a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+      <?php include("./component/pagination.php"); ?>
+    </div>
+  </section>
+  <!-- photo gallery page end -->
+
+
+  <!-- footer part start -->
+  <?php include("./component/footer.php"); ?>
+  <!-- footer part end -->
+
+
+  <!-- script part start -->
+  <?php include("./component/script.php"); ?>
+  <!-- script part end -->
+
+</body>
+
+</html>
